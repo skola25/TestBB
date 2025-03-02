537fb4682b07ae8b930752d93eb0b9db4e502754
skola25 <suneethakola1@gmail.com>
2025-03-02 18:33:08 -0500

    Add files via upload

---
+++ Insert_SALESPERSON.sql
@@ -0,0 +1,11 @@
+Insert into Salesperson 
+values('John', 'Smith', '123 ABC St', '123-123-1234', '2025-03-01', '2026-03-01', 'David Williams', 'SP001')
+
+Insert into Salesperson 
+values('Jason', 'Cooper', '234, Bradley St', '342-543-1234', '2025-01-26', '2026-01-26', 'David Williams', 'SP002')
+
+INSERT INTO [dbo].[SALESPERSON]
+VALUES('Mike','Steve','456, Main St','567-345-6787','2025-02-01 00:00:00.000','2026-02-01 00:00:00.000','David Williams','SP003')
+
+GO
+
