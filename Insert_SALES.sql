b322906fbb91e50f59e43f23d23111eda2fa4999
skola25 <suneethakola1@gmail.com>
2025-03-02 18:32:51 -0500

    Add files via upload

---
+++ Insert_SALES.sql
@@ -0,0 +1,58 @@
+
+INSERT INTO [dbo].[SALES]
+           ([prod_id]
+           ,[sp_id]
+           ,[cust_id]
+           ,[Sales_Date])
+     VALUES
+           ('P002'
+           ,'SP002'
+		   ,'C002'
+           ,'2025-02-28 00:00:00.000')
+
+INSERT INTO [dbo].[SALES]
+           ([prod_id]
+           ,[sp_id]
+           ,[cust_id]
+           ,[Sales_Date])
+     VALUES
+           ('P001'
+           ,'SP001'
+		   ,'C001'
+           ,'2025-03-01 17:40:20.580')
+
+
+INSERT INTO [dbo].[SALES]
+           ([prod_id]
+           ,[sp_id]
+           ,[cust_id]
+           ,[Sales_Date])
+     VALUES
+           ('P003'
+           ,'SP001'
+           ,'C002'
+           ,'2025-03-02 17:11:54.720')
+
+INSERT INTO [dbo].[SALES]
+           ([prod_id]
+           ,[sp_id]
+           ,[cust_id]
+           ,[Sales_Date])
+     VALUES
+           ('P003'
+           ,'SP001'
+           ,'C001'
+           ,'2025-03-02 17:13:30.383')
+
+INSERT INTO [dbo].[SALES]
+           ([prod_id]
+           ,[sp_id]
+           ,[cust_id]
+           ,[Sales_Date])
+     VALUES
+           ('P003'
+           ,'P002'
+           ,'C001'
+           ,'2025-03-02 17:11:54.720')
+
+GO
