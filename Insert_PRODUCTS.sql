cfc5689f4dcbf6b6872cc3ffe74c162f4be3932a
skola25 <suneethakola1@gmail.com>
2025-03-02 18:32:24 -0500

    Add files via upload

---
+++ Insert_PRODUCTS.sql
@@ -0,0 +1,60 @@
+
+INSERT INTO [dbo].[PRODUCTS]
+           ([prod_name]
+           ,[Manufacturer]
+           ,[Style]
+           ,[Purchase_Price]
+           ,[Sale_Price]
+           ,[Qty_On_Hand]
+           ,[Commission_Perc]
+           ,[prod_id])
+     VALUES
+           ('Bike1'
+           ,'ABC'
+           ,'B001'
+		   ,50.00
+           ,100.00
+           ,5
+           ,10
+           ,'P001')
+GO
+
+INSERT INTO [dbo].[PRODUCTS]
+           ([prod_name]
+           ,[Manufacturer]
+           ,[Style]
+           ,[Purchase_Price]
+           ,[Sale_Price]
+           ,[Qty_On_Hand]
+           ,[Commission_Perc]
+           ,[prod_id])
+     VALUES
+           ('Bike2'
+           ,'CDE'
+           ,'B001'
+		   ,60.00
+           ,100.00
+           ,3
+           ,10
+           ,'P002')
+GO
+
+INSERT INTO [dbo].[PRODUCTS]
+           ([prod_name]
+           ,[Manufacturer]
+           ,[Style]
+           ,[Purchase_Price]
+           ,[Sale_Price]
+           ,[Qty_On_Hand]
+           ,[Commission_Perc]
+           ,[prod_id])
+     VALUES
+           ('Bike3'
+           ,'EFG'
+           ,'B003'
+           ,70
+           ,80
+           ,3
+           ,3
+           ,'P003')
+GO
